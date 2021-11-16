5b0b0cdee29e375f07eb031a74ef912da86b5161
Abhishek Gopinath K <31348521+overtunned@users.noreply.github.com>
2021-11-16 19:37:11 +0530

    correct image_ids

--- Visual_All/image_feature_extractor.py
+++ Visual_All/image_feature_extractor.py
@@ -58,64 +58,63 @@ def image_feats_converter(filenames):
     with open(filenames['infile'], "r") as tsv_in_file:
         reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames=FIELDNAMES)
         for item in tqdm(reader):
-          try:
-              item['num_boxes'] = int(item['num_boxes'])
-              image_id = int(item['image_id'][1:])
-              image_w = float(item['image_w'])
-              image_h = float(item['image_h'])
-              bboxes = np.frombuffer(
-                  base64.b64decode(item['boxes']),
-                  dtype=np.float32).reshape((item['num_boxes'], -1))
+            item['num_boxes'] = int(item['num_boxes'])
+            try:
+                image_id = int(item['image_id'])
+            except:
+                continue
+            image_w = float(item['image_w'])
+            image_h = float(item['image_h'])
+            bboxes = np.frombuffer(
+                base64.b64decode(item['boxes']),
+                dtype=np.float32).reshape((item['num_boxes'], -1))
 
-              box_width = bboxes[:, 2] - bboxes[:, 0]
-              box_height = bboxes[:, 3] - bboxes[:, 1]
-              scaled_width = box_width / image_w
-              scaled_height = box_height / image_h
-              scaled_x = bboxes[:, 0] / image_w
-              scaled_y = bboxes[:, 1] / image_h
+            box_width = bboxes[:, 2] - bboxes[:, 0]
+            box_height = bboxes[:, 3] - bboxes[:, 1]
+            scaled_width = box_width / image_w
+            scaled_height = box_height / image_h
+            scaled_x = bboxes[:, 0] / image_w
+            scaled_y = bboxes[:, 1] / image_h
 
-              box_width = box_width[..., np.newaxis]
-              box_height = box_height[..., np.newaxis]
-              scaled_width = scaled_width[..., np.newaxis]
-              scaled_height = scaled_height[..., np.newaxis]
-              scaled_x = scaled_x[..., np.newaxis]
-              scaled_y = scaled_y[..., np.newaxis]
+            box_width = box_width[..., np.newaxis]
+            box_height = box_height[..., np.newaxis]
+            scaled_width = scaled_width[..., np.newaxis]
+            scaled_height = scaled_height[..., np.newaxis]
+            scaled_x = scaled_x[..., np.newaxis]
+            scaled_y = scaled_y[..., np.newaxis]
 
-              spatial_features = np.concatenate(
-                      (scaled_x,
-                      scaled_y,
-                      scaled_x + scaled_width,
-                      scaled_y + scaled_height,
-                      scaled_width,
-                      scaled_height),
-                      axis=1)
-              flag= False
-              if image_id in train_imgids:
-                  train_imgids.remove(image_id)
-                  train_indices[image_id] = train_counter
-                  train_img_bb[train_counter, :, :] = bboxes
-                  train_img_features[train_counter, :, :] = np.frombuffer(
-                      base64.b64decode(item['features']),
-                      dtype=np.float32).reshape((item['num_boxes'], -1))
-                  train_spatial_img_features[train_counter, :, :] = spatial_features
-                  train_counter += 1
-                  flag = True
-              if image_id in val_imgids:
-                  val_imgids.remove(image_id)
-                  val_indices[image_id] = val_counter
-                  val_img_bb[val_counter, :, :] = bboxes
-                  val_img_features[val_counter, :, :] = np.frombuffer(
-                      base64.b64decode(item['features']),
-                      dtype=np.float32).reshape((item['num_boxes'], -1))
-                  val_spatial_img_features[val_counter, :, :] = spatial_features
-                  val_counter += 1
-                  flag = True
-              if not flag:
-                  notfound.append(image_id)
-                  continue
-          except:
-            print(item['image_id'])
-            continue 
+            spatial_features = np.concatenate(
+                    (scaled_x,
+                    scaled_y,
+                    scaled_x + scaled_width,
+                    scaled_y + scaled_height,
+                    scaled_width,
+                    scaled_height),
+                    axis=1)
+            flag= False
+            if image_id in train_imgids:
+                train_imgids.remove(image_id)
+                train_indices[image_id] = train_counter
+                train_img_bb[train_counter, :, :] = bboxes
+                train_img_features[train_counter, :, :] = np.frombuffer(
+                    base64.b64decode(item['features']),
+                    dtype=np.float32).reshape((item['num_boxes'], -1))
+                train_spatial_img_features[train_counter, :, :] = spatial_features
+                train_counter += 1
+                flag = True
+            if image_id in val_imgids:
+                val_imgids.remove(image_id)
+                val_indices[image_id] = val_counter
+                val_img_bb[val_counter, :, :] = bboxes
+                val_img_features[val_counter, :, :] = np.frombuffer(
+                    base64.b64decode(item['features']),
+                    dtype=np.float32).reshape((item['num_boxes'], -1))
+                val_spatial_img_features[val_counter, :, :] = spatial_features
+                val_counter += 1
+                flag = True
+            if not flag:
+                notfound.append(image_id)
+                continue
 
     if len(train_imgids) != 0:
         print('Warning: train_image_ids is not empty')
